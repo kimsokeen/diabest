2042d371ed182764994dfa3dc87bffbc3f6f1e13
kimsokeen <keenmeka@gmail.com>
2025-05-06 19:17:08 +0700

    Update page9_instruction.py

--- pages/page9_instruction.py
+++ pages/page9_instruction.py
@@ -4,5 +4,5 @@ from helpers import back_button
 def page9():
     back_button(5)
     #st.title("วิธีการใช้งาน")
-    ins_path = r"C:\keen\project\project\project\assets\วิธีใช้งาน.jpg"
+    ins_path = "assets/วิธีใช้งาน.jpg"
     st.image(ins_path)
