6e5725a8619102bd49ee4488368a77458d0af8f6
kimsokeen <keenmeka@gmail.com>
2025-05-19 10:45:13 +0700

    Update auth.py

--- utils/auth.py
+++ utils/auth.py
@@ -19,7 +19,8 @@ def create_users_table():
             full_name TEXT NOT NULL,
             age INTEGER NOT NULL,
             gender TEXT NOT NULL,
-            tel TEXT NOT NULL
+            tel TEXT NOT NULL,
+            email TEXT NOT NULL
         )
     ''')
     conn.commit()
